c87077fdd8ac86ba89f22385d68bc3f4adfb17a4
Foram-Nandasana <89909997+Foram-Nandasana@users.noreply.github.com>
2022-09-12 13:47:44 +0530

    Add files via upload

--- src/main/webapps/src/component/Regform.js
+++ src/main/webapps/src/component/Regform.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form';
+import {Container} from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
 
 export default function Regform() {
   return (
-    <div><Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
+    <div> <Container>
+    <Form>
+    <Form.Label>Name</Form.Label>
+   <Form.Control type="text" placeholder="Enter name" />
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form></div>
+   <Form.Label>Email</Form.Label>
+   <Form.Control type="email" placeholder="Enter email" />
+
+   <Form.Label>Password</Form.Label>
+   <Form.Control type="password" placeholder="Enter password" />
+<br></br>
+ <Button variant="primary" type="submit">
+   Submit
+ </Button>
+</Form>
+</Container></div>
     
   )
 }
