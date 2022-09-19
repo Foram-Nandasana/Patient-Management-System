5c074cd243740e9a84e585e7da8d6ae81e11c5cf
Foram-Nandasana <89909997+Foram-Nandasana@users.noreply.github.com>
2022-09-19 12:54:08 +0530

    last commit

--- src/main/java/com/example/demo/errors/InvalidRequestException.java
+++ src/main/java/com/example/demo/errors/InvalidRequestException.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String s) {
+public class InvalidRequestException extends RuntimeException
+{
+    public InvalidRequestException(String s)
+    {
         super(s);
     }
 }
