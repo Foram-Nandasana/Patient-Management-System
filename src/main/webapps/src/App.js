165c6fc0227e8365f5830991bc9a419f030c6e91
Foram-Nandasana <89909997+Foram-Nandasana@users.noreply.github.com>
2022-09-19 12:34:57 +0530

    last commit

--- src/main/webapps/src/App.js
+++ src/main/webapps/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavigationBar from "./component/NavigationBar"
 import Footer from "./component/Footer";
+import Regform from './component/Regform';
 function App() {
   return (
     <div className="App">
         <NavigationBar/>
+        <Regform/>
         <Footer />
     </div>
   );
